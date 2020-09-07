698a71270c3ffc01b5795725343eb5fa017b56a2
Mariano <mariano.palacio@icloud.com>
2020-09-07 18:13:22 -0300

    Se modifico el archivo DB.py, el programa se ejecuta sin problemas.

--- DB.py
+++ DB.py
@@ -35,38 +35,4 @@ def create_connection():
         print(e)
 
     return conn
-
-
-def mostrar(tree):
-    # limpieza de tabla 
-    records = self.tree.get_children()
-    for element in records:
-        self.tree.delete(element)
-    # Consiguiendo datos
-    sql = 'SELECT * FROM producto ORDER BY id ASC'
-
-    conn = create_connection()
-    cur = conn.cursor()
-    cur.execute(sql)
-
-    resultado = cur.fetchall()
-
-    for fila in resultado:
-        print(fila)
-        self.tree.insert('', 0, text = fila[0], values = (fila[1],fila[2]))
-
 	
-def eliminardatos(tree):
-    id = self.tree.focus()
-    a = tree.item(id)
-    item = a['text']
-
-    self.tree.delete(id)
-    
-    conn = create_connection()
-    cur = conn.cursor()
-    sql = "DELETE from producto where Id =" + str(item)
-    cur.execute(sql)
-    conn.commit()   
-    
-    mostrar(self.tree)
